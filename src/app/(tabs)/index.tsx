b590d5a1dab167138089af713997ea429de63919
M Karthik Pai <mkarthikpai25@gmail.com>
2026-04-22 21:17:41 +0530

    Added code without Async storage

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import HomeHeader from "@/components/HomeHeader";
+import MacroGrid from "@/components/MacroGrid";
+import RecentMeals from "@/components/RecentMeals";
+import { globalStyles } from "@/styles/global";
+import { ScrollView, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+      <MacroGrid />
+      <RecentMeals />
+    </ScrollView>
+  );
+}
